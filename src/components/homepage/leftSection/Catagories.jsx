2c9a583c5655ca878f6a988b0180e331699514f6
tafsin90 <tafsin2003@gmail.com>
2026-06-20 08:22:38 +0600

    dhhh

--- src/components/homepage/leftSection/Catagories.jsx
+++ src/components/homepage/leftSection/Catagories.jsx
@@ -1,14 +1,17 @@
-import Link from 'next/link';
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
-const Catagories = ({ catagories,activeId}) => {
+const Catagories = ({ catagories, activeId }) => {
   return (
     <div>
       <h1 className="mb-2 font-bold text-lg">All Categories</h1>
       <ul className="flex flex-col gap-2">
         {catagories.map((catagory, index) => {
           return (
-            <li key={index} className={`${activeId === catagory.category_id && "bg-red-500"} text-md font-semibold text-center bg-slate-100 rounded-md hover:cursor-pointer hover:bg-slate-50`}>
+            <li
+              key={index}
+              className={`text-md font-semibold text-center rounded-md hover:cursor-pointer  ${activeId === catagory.category_id ? "bg-red-500 text-white" : "bg-slate-100 hover:bg-slate-50"}`}
+            >
               <Link href={`/catagory/${catagory.category_id}`} className="p-2 block">
                 {catagory.category_name}
               </Link>
@@ -20,4 +23,4 @@ const Catagories = ({ catagories,activeId}) => {
   );
 };
 
-export default Catagories
+export default Catagories;
